cc263dfc0c5db2913e66891c4b9d1f78379270f2
Parkashkumar797 <parkashkumar79738@gmail.com>
2024-06-29 22:02:18 +0530

    updated

---
+++ if-else-statement/script.js
@@ -0,0 +1,18 @@
+
+    function yellowColor(){
+        var A=parseInt(document.getElementById("in1").value)
+        var B=parseInt(document.getElementById("in2").value)
+ if(A>B){
+    document.getElementById("div1").style.backgroundColor="red"
+ }
+ else if(A<B){
+    document.getElementById("div1").style.backgroundColor="yellow"
+ }
+ else if(A==B){
+       document.getElementById("div1").style.backgroundColor="green"
+ }
+ else{
+    console.log("please enter a valid mumber");
+ }
+    }
+
